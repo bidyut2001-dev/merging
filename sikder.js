18e9f6aba9b742ceadb51794130c5d0f48547435
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2025-01-04 16:18:57 +0600

    Feture/adding apis (#16)
    * adding sikder.js
    
    * adding siker.js
    
    * apis:createing new apis
    
    * added new api2
    
    * added one conflict and changes on index

--- sikder.js
+++ sikder.js
@@ -2,10 +2,15 @@
 
 
 const dds=`Countfdfing objects: 100% (4/4), done.
-Delta compression using up to 12 threads
-Compressing objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 503 bytes | 503.00 KiB/s, done.
-Total 3 (delta fdfdasf0), reused 0 (delta 0), pack-reused 0
-To https://github.com/Bidyut-Sikder/merging.git
+Delta compressthe sign in, enter the verification code on the unrecognized device.
+
+Device: Chrome on Linux
+Verification code: 601978
+
+If you did not attempt to sign in to your account, your password may be compromised. Visit https://github.com/settings/security to create a new, strong password for your GitHub account.
+
+If you'd like to automatically verify devices in the future, consider enabling two-factor authentication on your account. Visit https://docs.github.com/articles/configuring-two-factor-authentication to learn about two-factor authentication.
+
+If you decide to enable two-factor authentication, ensure you retain access to one or more account recovery methods. See https://docs.github.com/articles/configuring-two-factor-authentication-recovery-methods in the GitHub Help.ng.git
    b88317b..137b838  feture/adding_teperal -> feture/adding_teperal`
 
