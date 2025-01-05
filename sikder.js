45a16c0c4ca51a408ee27011b19518b1bcd247da
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2025-01-05 08:32:09 +0600

    feture/changing_temperal_string changed successfully. (#21)
    * feture/changing_temperal_string changed successfully.
    
    * 2ns time
    
    * 3rd time

--- sikder.js
+++ sikder.js
@@ -1,7 +1,11 @@
 
 
 
-const dds=`Countfdfing objects: 100% (4/4), done.
+const dds=`
+this is added 3rd time
+this is added 2nd time
+this text is changed brother
+Countfdfing objects: 100% (4/4), done.
 Delta compressthe sign in, enter the verification code on the unrecognized device.
 
 Device: Chrome on Linux
